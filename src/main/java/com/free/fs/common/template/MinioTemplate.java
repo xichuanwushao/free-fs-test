0ea809d29a6dab2b0d8e19b4fe341244c34f8ccf
xichuanwushao <avatarwx@163.com>
2022-03-17 16:55:51 +0800

    4.9.5 对接wang jun的minio上传接口 1.存在问题 删除 下载文件目前不可以 objectName传参不对 未修改 2.上传文件可以 但是打不开

--- src/main/java/com/free/fs/common/template/MinioTemplate.java
+++ src/main/java/com/free/fs/common/template/MinioTemplate.java
@@ -1,7 +1,14 @@
 package com.free.fs.common.template;
 
+import cn.hutool.core.io.StreamProgress;
+import com.free.fs.common.minio.MinioClientBuilder;
 import com.free.fs.common.properties.FsServerProperties;
+import com.free.fs.common.util.FileUtil;
 import com.free.fs.model.FilePojo;
+import com.free.fs.common.util.MinioUtils;
+import io.minio.MinioClient;
+import io.minio.ObjectStat;
+import lombok.SneakyThrows;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +16,12 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
- * @author : wuxiao
- * @date : 20:40 2022/3/11
+ * @author : wangjun
+ * @date : 12:14 2022/3/16
  */
 @ConditionalOnProperty(prefix = "fs.files-server", name = "type", havingValue = "minio")
 @Component
@@ -20,23 +29,132 @@ public class MinioTemplate {
     @Resource
     private FsServerProperties fileProperties;
 
-    public void delete(String url) {
-        String key = fileProperties.getMinio().getAccessKey() ;
-        System.out.println("20:25 2022/3/11 MinioTemplate delete" +key);
-        System.out.println("还未实现！delete");
-    }
 
+    @SneakyThrows
+    public boolean delete(String objectName) {
+        MinioClient minioClient = new MinioClientBuilder().build(
+                fileProperties.getMinio().getConsoleUrl(),
+                fileProperties.getMinio().getApiPort(),
+                fileProperties.getMinio().getAccessKey(),
+                fileProperties.getMinio().getSecretKey(),false
+        );
+        Boolean isDeleteObject = MinioUtils.getInstance().deleteObject(minioClient,fileProperties.getMinio().getDefaultBucket(), objectName);
+        return isDeleteObject;
+    }
+    @SneakyThrows
     public FilePojo upload(MultipartFile file) {
-        System.out.println("还未实现！upload");
-        return null;
+        MinioClient minioClient = new MinioClientBuilder().build(
+                fileProperties.getMinio().getConsoleUrl(),
+                fileProperties.getMinio().getApiPort(),
+                fileProperties.getMinio().getAccessKey(),
+                fileProperties.getMinio().getSecretKey(),false
+        );
+        FilePojo pojo = FileUtil.buildFilePojo(file);
+        if (uploadObject(minioClient,file,pojo)) {
+            pojo.setUrl(getObjectUrl(file,pojo));
+        }
+        return pojo;
     }
 
     public FilePojo uploadSharding(MultipartFile file, HttpSession session) {
-        System.out.println("还未实现！uploadSharding");
+        System.out.println("还未实现！ uploadSharding" );
         return null;
     }
 
-    public void download(String url, HttpServletResponse response) {
-        System.out.println("还未实现！download");
+    public void download(String objectName, HttpServletResponse response) {
+        try {
+            MinioClient minioClient = new MinioClientBuilder().build(
+                    fileProperties.getMinio().getConsoleUrl(),
+                    fileProperties.getMinio().getApiPort(),
+                    fileProperties.getMinio().getAccessKey(),
+                    fileProperties.getMinio().getSecretKey(),false
+            );
+            ObjectStat objectStat = MinioUtils.getInstance().statObject(minioClient,fileProperties.getMinio().getDefaultBucket(), objectName);
+            long totalLength = objectStat.length();
+            MinioUtils.getInstance().downloadObject(minioClient,fileProperties.getMinio().getDefaultBucket(), objectName, response.getOutputStream(), 10240, new StreamProgress() {
+                @Override
+                public void start() {
+                    System.out.println("*****start*****");
+                }
+
+                @Override
+                public void progress(long readLength) {
+                    long rate = readLength * 100 / totalLength;
+                    System.out.println(rate);
+                }
+
+                @Override
+                public void finish() {
+                    System.out.println("*****finish*****");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+    public void download(String objectName, OutputStream outputStream) {
+        try {
+            MinioClient minioClient = new MinioClientBuilder().build(
+                    fileProperties.getMinio().getConsoleUrl(),
+                    fileProperties.getMinio().getApiPort(),
+                    fileProperties.getMinio().getAccessKey(),
+                    fileProperties.getMinio().getSecretKey(),false
+            );
+            ObjectStat objectStat = MinioUtils.getInstance().statObject(minioClient,fileProperties.getMinio().getDefaultBucket(), objectName);
+            long totalLength = objectStat.length();
+            MinioUtils.getInstance().downloadObject(minioClient,fileProperties.getMinio().getDefaultBucket(), objectName, outputStream, 10240, new StreamProgress() {
+                @Override
+                public void start() {
+                    System.out.println("*****start*****");
+                }
+
+                @Override
+                public void progress(long readLength) {
+                    long rate = readLength * 100 / totalLength;
+                    System.out.println(rate);
+                }
+
+                @Override
+                public void finish() {
+                    System.out.println("*****finish*****");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean uploadObject( MinioClient minioClient,MultipartFile file, FilePojo pojo) {
+        boolean isUpload = false;
+        long totalLength = file.getSize();
+        long partLength = 1024 * 1024 *5;   //5M
+        try {
+            InputStream inputStream = file.getInputStream();
+            isUpload = MinioUtils.getInstance().uploadFile(minioClient,fileProperties.getMinio().getDefaultBucket(), pojo.getFileName(), inputStream, pojo.getSize(), partLength);
+            // 关闭流
+            inputStream.close();
+        } catch (Exception e) {
+            isUpload = false;
+            e.printStackTrace();
+        }
+        return isUpload;
+    }
+    @SneakyThrows
+    public String getObjectUrl(MultipartFile file,FilePojo pojo) {
+        MinioClient minioClient = new MinioClientBuilder().build(
+                fileProperties.getMinio().getConsoleUrl(),
+                fileProperties.getMinio().getApiPort(),
+                fileProperties.getMinio().getAccessKey(),
+                fileProperties.getMinio().getSecretKey(),false
+        );
+        String objectName = file.getOriginalFilename();
+        String objectUrl = MinioUtils.getInstance().getPresignedObjectUrl(minioClient,fileProperties.getMinio().getDefaultBucket(), pojo.getFileName(), 3600);
+        System.out.println("对象的外链URL=="+objectUrl);
+        return objectUrl;
+    }
+
+
 }
--- src/main/java/com/free/fs/service/impl/MinioService.java
+++ src/main/java/com/free/fs/service/impl/MinioService.java
@@ -1,7 +1,6 @@
 package com.free.fs.service.impl;
 
 import com.free.fs.common.template.MinioTemplate;
-import com.free.fs.common.template.OssTemplate;
 import com.free.fs.model.FilePojo;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -12,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * @author : wuxiao
- * @date : 20:58 2022/3/11
+ * @author : wangjun
+ * @date : 12:14 2022/3/16
  */
 @Service
 @ConditionalOnProperty(prefix = "fs.files-server", name = "type", havingValue = "minio")
@@ -25,7 +24,8 @@ public class MinioService extends AbstractIFileService{
     @Override
     protected FilePojo uploadFile(MultipartFile file) {
         System.out.println("20:25 2022/3/11 MinioService uploadFile" );
-        return minioTemplate.upload(file);
+        FilePojo filePojo = minioTemplate.upload(file);
+        return filePojo;
     }
 
     @Override
